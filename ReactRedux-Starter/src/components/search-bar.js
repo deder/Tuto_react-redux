09469d09878076079c4e6e64e79c5c45767bf8da
fcosta <fcosta@deder.fr>
2018-08-14 11:52:20 +0200

    Ajout d'un handler sur l'input de SearchBar e tmise en place de deux states : searchText && placeHolder

--- ReactRedux-Starter/src/components/search-bar.js
+++ ReactRedux-Starter/src/components/search-bar.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <input />
+class SearchBar extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            searchText:"",
+            placeHolder:"Entrer le nom d'un film"
+        }
+    }
+    render(){
+        return (
+            <div>
+                <input onChange={this.searchHandler} value={this.state.searchText} placeholder={this.state.placeHolder}/>
+                {this.state.searchText}
+            </div>
+        );
+    }
+    searchHandler = (event) => {
+        this.setState(
+            {
+                searchText : event.target.value
+            }
+        );
+    }
 }
 export default SearchBar;
