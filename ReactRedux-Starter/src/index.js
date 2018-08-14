fca52bc0a60284139c80b142c9b518e5569a1e9f
fcosta <fcosta@deder.fr>
2018-08-14 11:18:31 +0200

    Séparation du rendu de l'application et de la construction de l'application dans les fichiers index.js et app.js

--- ReactRedux-Starter/src/index.js
+++ ReactRedux-Starter/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import IhmConfig from './configs/ihm.config'
+import App from './containers/app'
 
-const App = function(){
-    return <div>Test</div>;
-}
+const API_KEY = IhmConfig.API_KEY;
 
 ReactDom.render(<App />, document.querySelector(".container"));
