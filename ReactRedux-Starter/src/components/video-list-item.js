5a85737bb101fe0d2f22ef072bf6cf7ddee2c5d9
fcosta <fcosta@deder.fr>
2018-08-14 15:52:36 +0200

    Simplification du composant video-list-item

--- ReactRedux-Starter/src/components/video-list-item.js
+++ ReactRedux-Starter/src/components/video-list-item.js
@@ -2,23 +2,12 @@ import React, {Component} from 'react';
 import { ReactDom } from 'react-dom';
 import moment from 'moment'
 moment.locale("fr");
-class VideoListItem extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            title:props.title,
-            release_date:props.release_date
-        };
-    }
-    componentWillReceiveProps(props){
-        this.setState({
-            title:props.title,
-            release_date:props.release_date
-        });
-    }
-    render(){
-        return (<li>{this.state.title} || Sorti {moment(this.state.release_date,"YYYY-MM-DD").fromNow()}</li>);
-    }
+const VideoListItem = ({title, release_date}) => {
+    return (
+        <li>
+            {title} || Sorti {moment(release_date,"YYYY-MM-DD").fromNow()}
+        </li>
+    );
 }
 
 export default VideoListItem;
--- ReactRedux-Starter/src/containers/app.js
+++ ReactRedux-Starter/src/containers/app.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 import SearchBar from '../components/search-bar';
 import VideoList from './video-list';
+import videoService from '../services/video.service';
 
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-            <VideoList />
-        </div>
-    ) ;
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            popularMovies:[]
+        };
+    }
+    componentWillMount(){
+        videoService.getPopularMovies().then((res)=>{
+            console.log(res.data.results);
+            this.setState({
+                popularMovies:res.data.results.slice(1,6)
+            });
+        })
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.popularMovies}/>
+            </div>
+        );
+    }
 }
 
 export default App;
