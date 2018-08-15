b44c022af33d2a403c11ac6732f57975cf7d53ea
deder <fcosta@deder.fr>
2018-08-15 13:10:02 +0200

    Mise en place des posters de film dans video-list-item

--- ReactRedux-Starter/src/components/video-list-item.js
+++ ReactRedux-Starter/src/components/video-list-item.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import { ReactDom } from 'react-dom';
+
 import moment from 'moment'
+import IhmConfig from '../configs/ihm.config';
 moment.locale("fr");
-const VideoListItem = ({title, release_date}) => {
+const VideoListItem = ({title, release_date, poster_path}) => {
     return (
         <li>
+            <img width="100px" src={`${IhmConfig.IMAGE_BASE_URL}${poster_path}`} />
             {title} || Sorti {moment(release_date,"YYYY-MM-DD").fromNow()}
         </li>
     );
--- ReactRedux-Starter/src/containers/video-list.js
+++ ReactRedux-Starter/src/containers/video-list.js
@@ -3,8 +3,8 @@ import VideoListItem from './../components/video-list-item';
 
 const VideoList = ({videos}) => {
     const VideoListItems = [];
-    for(let {id, title, release_date} of videos){
-        VideoListItems.push(<VideoListItem key={id} title={title} release_date={release_date} />);
+    for(let {id, title, release_date, poster_path} of videos){
+        VideoListItems.push(<VideoListItem key={id} title={title} release_date={release_date} poster_path={poster_path} />);
     }
     return (<ul>
         {VideoListItems}
