46762a99548d3b315d5de9fdcf1657565a3997d6
fcosta <fcosta@deder.fr>
2018-08-17 16:44:39 +0200

    Ajour de la mise en page de video-list.js

--- ReactRedux-Starter/src/containers/video-list.js
+++ ReactRedux-Starter/src/containers/video-list.js
@@ -3,12 +3,32 @@ import VideoListItem from './../components/video-list-item';
 
 const VideoList = ({videos}) => {
     const VideoListItems = [];
+    let indexItem = 0;
     for(let {id, title, release_date, poster_path} of videos){
-        VideoListItems.push(<VideoListItem key={id} title={title} release_date={release_date} poster_path={poster_path} />);
+        VideoListItems.push(<VideoListItem index={indexItem} key={id} title={title} release_date={release_date} poster_path={poster_path} />);
+        indexItem++
     }
-    return (<ul>
-        {VideoListItems}
-    </ul>);
+    return (
+        <div className="row">
+            <div className="col s12" style={{
+            }}>
+                <span style={{
+                    padding:"24px",
+                    paddingLeft:0,
+                    display: "block",
+                    fontSize: "28px",
+                    fontWeight: "bold",
+                    color:"grey",
+                    opacity:0.7
+                }}>
+                    Films populaires
+                </span>
+            </div>
+            <div className="col s12 row">
+                {VideoListItems}
+            </div>
+        </div>
 
+    );
 }
 export default VideoList;
