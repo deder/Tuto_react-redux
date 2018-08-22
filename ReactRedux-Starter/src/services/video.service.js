9a31ee5c0c86ff079dbe00529a5ff3dabfb80bc4
fcosta <fcosta@deder.fr>
2018-08-22 17:32:18 +0200

    Création d'une méthoe service permetant de récupérer les vidéos recommandés pour une film spécifique

--- ReactRedux-Starter/src/services/video.service.js
+++ ReactRedux-Starter/src/services/video.service.js
@@ -5,6 +5,7 @@ class VideoService {
     urlsService = {
         popularMovies: `${IhmConfig.API_END_POINT}discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&api_key=${IhmConfig.API_KEY}`,
         urlVideoMovie: `${IhmConfig.API_END_POINT}movie/[id]?language=fr&append_to_response=videos&include_adult=false&api_key=${IhmConfig.API_KEY}`,
+        urlRecommendationsVideoMovie: `${IhmConfig.API_END_POINT}movie/[id]/recommendations?language=fr&append_to_response=videos&include_adult=false&api_key=${IhmConfig.API_KEY}`,
         urlSearchMovie: `${IhmConfig.API_END_POINT}search/movie?query=[searchTxt]&language=fr&include_adult=false&api_key=${IhmConfig.API_KEY}`
     }
     getPopularMovies = () => {
@@ -17,6 +18,10 @@ class VideoService {
     getSearchMovie = (searchTxt) => {
         console.log(this.urlsService.urlSearchMovie.replace("[searchTxt]",searchTxt))
         return axios.get(this.urlsService.urlSearchMovie.replace("[searchTxt]",searchTxt))
+    }
+    getRecommendationsMovie = (id) => {
+        console.log(this.urlsService.urlSearchMovie.replace("[id]",id))
+        return axios.get(this.urlsService.urlRecommendationsVideoMovie.replace("[id]",id))
     }
 }
 
