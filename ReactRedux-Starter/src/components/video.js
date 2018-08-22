44667fe565a37d413c2494507f89a073ccf6e3dd
fcosta <fcosta@deder.fr>
2018-08-22 14:32:06 +0200

    Ajout de la propriété with au composant video.js

--- ReactRedux-Starter/src/components/video.js
+++ ReactRedux-Starter/src/components/video.js
@@ -1,18 +1,18 @@
 import React from "react";
 import YouTube from "react-youtube"
-const Video = ({videoId}) => {
+const Video = ({videoId, width}) => {
     const opts = {
         height: '390',
-        width: '640',
+        width: width,
         playerVars: { // https://developers.google.com/youtube/player_parameters
             autoplay: 0,   
             iv_load_policy: 3,
             modestbranding: 1,
             rel:0,
             showinfo:0
-        },
-
+        }
     };
+    console.log(videoId)
     return (
         <YouTube videoId={videoId} opts={opts} />
     );
