32c025517dce957253b106698fd53745d22d5bf4
fcosta <fcosta@deder.fr>
2018-08-23 17:13:07 +0200

    Fix du composant user_list & intégration du composant métier à app.js afin de l'afficher

--- ReactRedux-User/src/components/user_list.js
+++ ReactRedux-User/src/components/user_list.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import User from './user';
-import CollectionLink from './materializeCSS/collection/collection-link';
+import CollectionLink from './materializeCSS/collections/collection-link';
 import Utils from './materializeCSS/utils/utils';
-import CollectionItemAvatar from './materializeCSS/collection/collection-item-avatar';
-import CollectionItem from './materializeCSS/collection/collection-item';
+import CollectionItemAvatar from './materializeCSS/collections/collection-item-avatar';
+import CollectionItem from './materializeCSS/collections/collection-item';
 
 const UserList = ({className=[], children=[]}) => {
     className = Utils.forcedClassNameToArray(className);
