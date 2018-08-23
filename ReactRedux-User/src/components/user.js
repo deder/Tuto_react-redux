34e27aba82f4e2a9110eee5d67ee0810901db111
fcosta <fcosta@deder.fr>
2018-08-23 17:31:41 +0200

    Changement du fichier user afin de lui ajouter un avatar

--- ReactRedux-User/src/components/user.js
+++ ReactRedux-User/src/components/user.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import CollectionItemLink from './materializeCSS/collections/collection-item-link';
+import CollectionItemAvatar from './materializeCSS/collections/collection-item-avatar';
 import Utils from './materializeCSS/utils/utils';
 
 
 
 const User = ({className=[], children=[]}) => {
     className = Utils.forcedClassNameToArray(className);
-    return (<CollectionItemLink className={className.join(" ")} >{children}</CollectionItemLink>);
+    return (<CollectionItemAvatar className={className.join(" ")} icon="face">{children}</CollectionItemAvatar>);
 }
 
 export default User; 
