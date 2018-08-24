83e9b862f545f00a0347b143be7fffcbdfd75ad1
fcosta <fcosta@deder.fr>
2018-08-24 11:51:37 +0200

    ajout du onCLick au composant

--- ReactRedux-User/src/components/user.js
+++ ReactRedux-User/src/components/user.js
@@ -2,11 +2,9 @@ import React from 'react';
 import CollectionItemAvatar from './materializeCSS/collections/collection-item-avatar';
 import Utils from './materializeCSS/utils/utils';
 
-
-
-const User = ({className=[], children=[]}) => {
+const User = ({className=[], children=[], onClick}) => {
     className = Utils.forcedClassNameToArray(className);
-    return (<CollectionItemAvatar className={className.join(" ")} icon="face">{children}</CollectionItemAvatar>);
+    return (<CollectionItemAvatar {...{onClick}} className={className.join(" ")} icon="face">{children}</CollectionItemAvatar>);
 }
 
 export default User; 
