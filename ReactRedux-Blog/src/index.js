ae09ef140650d55e12ce7078f5e42af86397e131
fcosta <fcosta@deder.fr>
2018-08-28 15:01:12 +0200

    Ajout du module thunk au projet

--- ReactRedux-Blog/src/index.js
+++ ReactRedux-Blog/src/index.js
@@ -4,9 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import Routes from './routes';
+import thunk from '../node_modules/redux-thunk';
 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
