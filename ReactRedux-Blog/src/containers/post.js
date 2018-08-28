dd36cc059c958f527cd21bd8eacbec3a8a35dd2e
fcosta <fcosta@deder.fr>
2018-08-28 14:59:45 +0200

    Ajout des routes * et /post/:id

--- ReactRedux-Blog/src/containers/post.js
+++ ReactRedux-Blog/src/containers/post.js
@@ -3,8 +3,10 @@ import PostContent from '../components/post-content';
 
 export default class Post extends Component {
   render() {
+      console.log(this.props)
     return (
       <div>
+          Numéro de post : {this.props.match.params.id}
         <PostContent></PostContent>
       </div>
     )
--- ReactRedux-Blog/src/routes.js
+++ ReactRedux-Blog/src/routes.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import PostList from './containers/post-list';
 import PostForm from './containers/post-form';
+import Post from './containers/post';
+import NotFound from './components/not-found';
 export default class Routes extends Component {
   render() {
     return (
@@ -9,6 +11,8 @@ export default class Routes extends Component {
             <Switch>
                 <Route exact path='/' component={PostList} />
                 <Route path='/create-post' component={PostForm} />
+                <Route path="/post/:id" component={Post} />
+                <Route path="*" component={NotFound} />
             </Switch>
         </BrowserRouter>
     )
