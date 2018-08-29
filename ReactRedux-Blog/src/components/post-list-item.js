e655a5464d0cb822c26705f3cc1de75d27b6b8a6
fcosta <fcosta@deder.fr>
2018-08-29 10:48:38 +0200

    Mise en place du composant post-list et intégration de ce composant dans post-list-item

--- ReactRedux-Blog/src/components/post-list-item.js
+++ ReactRedux-Blog/src/components/post-list-item.js
@@ -1,9 +1,38 @@
 import React from 'react'
 
-export default () => {
-  return (
-    <div>
-      <p>Element de la liste</p>
+
+const fontLittleStyle = {
+  fontSize:"12px"
+}
+
+const relativeStyle = {
+  position: "relative"
+}
+const floatingBtnStyle = {
+  top : "-10px",
+  right: "0"
+}
+
+
+const PostListItem = ({ post }) => {
+  const { title, content, author, date } = post;
+  console.log(title);
+  return(
+    <div style={relativeStyle} className="row col s12">
+      <a style={floatingBtnStyle} className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">add</i></a>
+      <div className="col s12">
+        <div className="card blue-grey darken-1">
+          <div className="card-content white-text row">
+            <span className="card-title col s8">{title}</span>
+            <span style={fontLittleStyle} className="card-title col s4 right-align">{date}</span>
+            <p>{content}</p>
+            <span style={fontLittleStyle} className="card-title col s12 right-align">{author}</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
+
+
+export default PostListItem;
--- ReactRedux-Blog/src/containers/post-list.js
+++ ReactRedux-Blog/src/containers/post-list.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import { readAllPost } from './../actions/index';
+import PostListItem from '../components/post-list-item';
 
-export default class PostList extends Component {
+const fontLittleStyle = {
+  fontSize:"12px"
+}
+
+class PostList extends Component {
+  componentWillMount() {
+    this.props.readAllPost();
+  }
+  hasPosts = () => {
+    if (this.props.posts && this.props.posts.length > 0)
+      return true
+
+    return false;
+  }
+  renderPosts = () => {
+    if (this.hasPosts()) {
+      return this.props.posts.map((post) => {
+        return (
+          <PostListItem key={post.id} {...{post}}/>
+        );
+      });
+    }
+    return "";
+  }
   render() {
     return (
       <div>
-          <h1>Liste des posts</h1>
+        <h1>Liste des posts</h1>
+        <div className="row">{this.renderPosts()}</div>
       </div>
     )
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ readAllPost }, dispatch);
+}
+export default connect(mapStateToProps, mapDispatchToProps)(PostList)
